cace39516dc3501bd8584035df58827e6c06313a
David Lorente <david.l.castilla@gmail.com>
2018-06-13 20:52:35 -0400

    add mta request

--- bindings/python/samples/test.py
+++ bindings/python/samples/test.py
@@ -4,6 +4,15 @@ from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
 import requests
+from google.transit import gtfs_realtime_pb2
+from google.protobuf.json_format import MessageToJson
+import requests
+import os # imports package for dotenv
+import json
+
+api_key = '39fa1b3cfea1c72849864463ea04e331'
+
+stop_id = 'L15'
 
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
@@ -19,19 +28,21 @@ class RunText(SampleBase):
         my_text = self.args.text
         n = 0
         while True:
-            r = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/spot')
-            price = "BTC: " + r.json()['data']['amount']
+            times = get_times()
+            # r = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/spot')
+            # price = "BTC: " + r.json()['data']['amount']
+            timestr = times["N"][0] + " MINUTES";
 
             offscreen_canvas.Clear()
             # len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-            len = graphics.DrawText(offscreen_canvas, font, 0, 10, textColor, price)
+            len = graphics.DrawText(offscreen_canvas, font, 0, 10, textColor, timestr)
             n += 1
 
             # pos -= 1
             # if (pos + len < 0):
             #     pos = offscreen_canvas.width
 
-            time.sleep(0.5)
+            time.sleep(15)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
@@ -40,3 +51,96 @@ if __name__ == "__main__":
     run_text = RunText()
     if (not run_text.process()):
         run_text.print_help()
+
+
+
+# from google.transit import gtfs_realtime_pb2
+# from google.protobuf.json_format import MessageToJson
+# import requests
+# import time # imports module for Epoch/GMT time conversion
+# import os # imports package for dotenv
+# import json
+
+# api_key = '39fa1b3cfea1c72849864463ea04e331'
+
+# stop_id = 'L15'
+
+def get_times():
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get('http://datamine.mta.info/mta_esi.php?key={}&feed_id=2'.format(api_key))
+    feed.ParseFromString(response.content)
+
+    jsonObj = MessageToJson(feed)
+    data = json.loads(jsonObj)
+
+    stops = {
+        "S": [],
+        "N": []
+    }
+
+    time_now = int(time.time())
+
+    for entity in data['entity']:
+        if('tripUpdate' in entity):
+            for sched in entity['tripUpdate']['stopTimeUpdate']:
+                if( stop_id in sched['stopId']):
+
+                    stops[sched['stopId'].replace(stop_id, "")].append( round((int(sched['arrival']['time']) - time_now) / 60) )
+
+
+    return stops
+
+# while True:
+#     print(get_times())
+#     time.sleep(15)
+
+
+
+
+
+
+
+
+
+# #!/usr/bin/env python
+# # Display a runtext with double-buffering.
+# from samplebase import SampleBase
+# from rgbmatrix import graphics
+# import time
+# import requests
+
+# class RunText(SampleBase):
+#     def __init__(self, *args, **kwargs):
+#         super(RunText, self).__init__(*args, **kwargs)
+#         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
+
+#     def run(self):
+#         offscreen_canvas = self.matrix.CreateFrameCanvas()
+#         font = graphics.Font()
+#         font.LoadFont("../../../fonts/7x13.bdf")
+#         textColor = graphics.Color(255, 255, 0)
+#         pos = offscreen_canvas.width
+#         my_text = self.args.text
+#         n = 0
+#         while True:
+#             r = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/spot')
+#             price = "BTC: " + r.json()['data']['amount']
+
+#             offscreen_canvas.Clear()
+#             # len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+#             len = graphics.DrawText(offscreen_canvas, font, 0, 10, textColor, price)
+#             n += 1
+
+#             # pos -= 1
+#             # if (pos + len < 0):
+#             #     pos = offscreen_canvas.width
+
+#             time.sleep(0.5)
+#             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
+
+# # Main function
+# if __name__ == "__main__":
+#     run_text = RunText()
+#     if (not run_text.process()):
+#         run_text.print_help()
