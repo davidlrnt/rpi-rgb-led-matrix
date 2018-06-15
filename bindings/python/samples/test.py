bd72fa4291cbd46083451779847243e2f7fd754f
David Lorente <david.l.castilla@gmail.com>
2018-06-14 20:44:16 -0400

    str()

--- bindings/python/samples/test.py
+++ bindings/python/samples/test.py
@@ -56,7 +56,7 @@ class RunText(SampleBase):
             times = get_times()
             # r = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/spot')
             # price = "BTC: " + r.json()['data']['amount']
-            timestr = times["N"][0] + " MINUTES";
+            timestr = str(times["N"][0]) + " MINUTES";
 
             offscreen_canvas.Clear()
             # len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
