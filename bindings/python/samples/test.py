ecb6e79f6da2eaed48e23c41669b0101a1ef1590
David Lorente <david.l.castilla@gmail.com>
2018-06-14 20:43:09 -0400

    refactor

--- bindings/python/samples/test.py
+++ bindings/python/samples/test.py
@@ -14,6 +14,31 @@ api_key = '39fa1b3cfea1c72849864463ea04e331'
 
 stop_id = 'L15'
 
+def get_times():
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get('http://datamine.mta.info/mta_esi.php?key={}&feed_id=2'.format(api_key))
+    feed.ParseFromString(response.content)
+
+    jsonObj = MessageToJson(feed)
+    data = json.loads(jsonObj)
+
+    stops = {
+        "S": [],
+        "N": []
+    }
+
+    time_now = int(time.time())
+
+    for entity in data['entity']:
+        if('tripUpdate' in entity):
+            for sched in entity['tripUpdate']['stopTimeUpdate']:
+                if( stop_id in sched['stopId']):
+
+                    stops[sched['stopId'].replace(stop_id, "")].append( round((int(sched['arrival']['time']) - time_now) / 60) )
+
+
+    return stops
+
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
@@ -22,7 +47,7 @@ class RunText(SampleBase):
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont("../../../fonts/7x13.bdf")
+        font.LoadFont("../../../fonts/5x8.bdf")
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
         my_text = self.args.text
@@ -65,30 +90,7 @@ if __name__ == "__main__":
 
 # stop_id = 'L15'
 
-def get_times():
-    feed = gtfs_realtime_pb2.FeedMessage()
-    response = requests.get('http://datamine.mta.info/mta_esi.php?key={}&feed_id=2'.format(api_key))
-    feed.ParseFromString(response.content)
 
-    jsonObj = MessageToJson(feed)
-    data = json.loads(jsonObj)
-
-    stops = {
-        "S": [],
-        "N": []
-    }
-
-    time_now = int(time.time())
-
-    for entity in data['entity']:
-        if('tripUpdate' in entity):
-            for sched in entity['tripUpdate']['stopTimeUpdate']:
-                if( stop_id in sched['stopId']):
-
-                    stops[sched['stopId'].replace(stop_id, "")].append( round((int(sched['arrival']['time']) - time_now) / 60) )
-
-
-    return stops
 
 # while True:
 #     print(get_times())
