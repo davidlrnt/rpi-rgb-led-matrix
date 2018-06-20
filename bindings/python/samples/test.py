a79d89a66a4f7d0fba2b5e7f50fd1b4b0f0143ea
David Lorente <david.l.castilla@gmail.com>
2018-06-19 20:49:41 -0400

    font s

--- bindings/python/samples/test.py
+++ bindings/python/samples/test.py
@@ -64,7 +64,7 @@ class RunText(SampleBase):
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont("../../../fonts/6x9.bdf")
+        font.LoadFont("../../../fonts/6x12.bdf")
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
         my_text = self.args.text
